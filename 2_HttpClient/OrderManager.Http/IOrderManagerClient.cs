a1df3e6f1b7195275635c38175e324e55f55ae21
Benjamin Born <benjamin.born@loewensteinmedical.de>
2019-08-30 08:38:02 +0200

    Add example for HttpClient

---
+++ 2_HttpClient/OrderManager.Http/IOrderManagerClient.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using OrderManager.Http.Dtos;
+
+namespace OrderManager.Http
+{
+    /// <summary>
+    /// Client to access the OrderManager service from other services.
+    /// </summary>
+    public interface IOrderManagerClient
+    {
+        /// <summary>
+        /// Creates an order.
+        /// </summary>
+        /// <param name="item">Product item</param>
+        /// <param name="count">Number of items.</param>
+        /// <returns>Unique id of the created order.</returns>
+        Task<int> CreateOrder(string item, int count);
+
+        /// <summary>
+        /// Gets the order by the given id.
+        /// </summary>
+        /// <param name="orderId">The order id.</param>
+        /// <returns>The Order</returns>
+        Task<OrderDto> GetOrder(int orderId);
+    }
+
+    class OrderManagerClient : IOrderManagerClient
+    {
+        private readonly HttpClient _client;
+
+        public OrderManagerClient(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<int> CreateOrder(string item, int count)
+        {
+            HttpResponseMessage response = await _client.PostAsJsonAsync("api/OrderManager", new CreateOrderDto
+            {
+                Count = count,
+                Item = item
+            });
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Order failed");
+            }
+
+            return Convert.ToInt32(await response.Content.ReadAsStringAsync());
+        }
+
+        public async Task<OrderDto> GetOrder(int orderId)
+        {
+            HttpResponseMessage response = await _client.GetAsync($"api/OrderManager/{orderId}");
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Failed to select order");
+            }
+            return await response.Content.ReadAsAsync<OrderDto>();
+        }
+    }
+}
---
+++ 2_HttpClient/OrderManager/Controllers/OrderManagerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using Microsoft.AspNetCore.Mvc;
+using OrderManager.Http.Dtos;
+
+namespace OrderManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderManagerController : ControllerBase
+    {
+        private int _orderSeq = 0;
+        private static readonly Dictionary<int, OrderDto> _orderStorage = new Dictionary<int, OrderDto>();
+
+        /// <summary>
+        /// Creates an order.
+        /// </summary>
+        /// <param name="dto">Order dto</param>
+        /// <returns>Unique id of the created order.</returns>
+        [HttpPost]
+        public int CreateOrder([FromBody] CreateOrderDto dto)
+        {
+            int id = Interlocked.Increment(ref _orderSeq);
+            _orderStorage[id] = new OrderDto
+            {
+                Id = id,
+                Count = dto.Count,
+                Item = dto.Item
+            };
+
+            return id;
+        }
+
+        /// <summary>
+        /// Gets the order by the given id.
+        /// </summary>
+        /// <param name="orderId">The order id.</param>
+        /// <returns>The Order</returns>
+        [HttpGet]
+        [Route("{orderId}")]
+        public OrderDto GetOrder([FromRoute]int orderId)
+        {
+            return _orderStorage[orderId];
+        }
+    }
+}
